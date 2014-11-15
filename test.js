615bbc40a319e35affe601cec229dd5827850a9c
Bob <suhussai@ualberta.ca>
2014-11-14 21:44:30 -0700

    some extra pics

---
+++ test.js
@@ -0,0 +1,11 @@
+function showSelection() {
+    document.getElementById('gameSelectionBox').css('visibility', 'visible'); 
+//    $("gameSelectionBox").css("visibility", "visible");
+
+}
+
+function hideSelection() {
+    document.getElementById('gameSelectionBox').css('visibility', 'hidden'); 
+//    $("gameSelectionBox").css("visibility", "visible");
+
+}
