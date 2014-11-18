fb5ec0fd13e626cf32bb6cdd4872f187f27176f8
suhussai <suhussai@ualberta.ca>
2014-11-18 14:21:34 -0700

    updated category title and edited the add game python script to include category and thumbnail insertions

--- addGame.py
+++ addGame.py
@@ -1,84 +1,72 @@
 print("Starting");
 
-gameFile = open("gameDB.js", "r+");
+'''
+Process for adding game:
+1. Get game title.
 
-gameInfo = gameFile.read();
-fileSize = len(gameInfo);
-gameInfo = gameInfo.split('\n');
+2. Get game category.
 
-gameTitleInfo = gameInfo[0];
-print(gameTitleInfo);
+3. Get game source.
 
-gameSourceInfo = gameInfo[1];
-print(gameSourceInfo);
-
-
-gameTitle = input("Enter game title: ");
-gameTitle = ', "' + gameTitle + '"];';
-
-gameSource = input("Enter game source: ");
-gameSource = ", '" + gameSource + "'];";
-
-newGameInfo = gameTitleInfo[0:len(gameTitleInfo)-2] + gameTitle + "\n" + gameSourceInfo[0:len(gameSourceInfo)-2] + gameSource;
-gameFile.seek(0);
-gameFile.truncate();
-gameFile.write(newGameInfo);
-print(newGameInfo);
+4. Get game thumbnail.
 
 '''
 
 
+def getTitle():    
+    gameTitle = input("Enter game title: ");
+    gameTitle = ', "' + gameTitle + '"];';
+    return gameTitle;
 
+def getSource():
+    gameSource = input("Enter game source: ");
+    gameSource = ", '" + gameSource + "'];";
+    return gameSource
 
+def getCategory():
+    gameCategory = input("Enter game category: ");
+    return gameCategory
 
+def getThumbnail():
+    gameThumbnail = input("Enter game thumbnail name: ");
+    return gameThumbnail
+    
+def writeCategoryAndThumbnail():
+    categoryFile = open("categories.js", "r+");
+    categoryInfo = categoryFile.read();
+    category = getCategory();
+    thumbnail = getThumbnail();
+    splitter = 'var for' + category + ' = ["", ';
+    categoryInfo = categoryInfo.split(splitter);
+    index = categoryInfo[1].find('""');
+    #there exists a chance where you might write more images than there is space
+    #beware
+    newCategoryInfo = categoryInfo[0] + splitter +  categoryInfo[1][0:index+1] + thumbnail + categoryInfo[1][index+1::];
+    categoryFile.seek(0);
+    categoryFile.truncate();
+    categoryFile.write(newCategoryInfo);
+    print("Done writing thumbnail in proper category");
 
+def writeTitleAndSource():
+    gameFile = open("gameDB.js", "r+");
+    gameInfo = gameFile.read();
+    gameInfo = gameInfo.split('\n');
+    
+    gameTitleInfo = gameInfo[0];
+    #print(gameTitleInfo);
+    
+    gameSourceInfo = gameInfo[1];
+    #print(gameSourceInfo);
+    
 
-#read old game index and game source index
-referenceFile.seek(0,0); # reset seeker first is offset, second is offset from what
-IndexFile = referenceFile.read();
-gameIndex = IndexFile.split('-')[0];
-sourceIndex = IndexFile.split('-')[1];
+    gameTitle = getTitle(); 
+    gameSource = getSource();
+    
+    newGameInfo = gameTitleInfo[0:len(gameTitleInfo)-2] + gameTitle + "\n" + gameSourceInfo[0:len(gameSourceInfo)-2] + gameSource;
+    gameFile.seek(0);
+    gameFile.truncate();
+    gameFile.write(newGameInfo);
+    
 
-gameIndex = int(gameIndex);
-sourceIndex = int(sourceIndex);
-
-print(gameIndex);
-print(sourceIndex);
-
-gameFile = open("gameDB.js", "r+");
-
-gameTitle = input("Enter game title: ");
-gameTitle = ', "' + gameTitle + '"';
-
-gameSource = input("Enter game source: ");
-gameSource = ", '" + gameSource + "'";
-
-leftPortionGI = gameFile.read(gameIndex);
-rightPortionGI = gameFile.read(
-
-
-gameFile.seek(gameIndex);
-gameFile.write(gameTitle);
-
-gameFile.seek(sourceIndex);
-gameFile.write(gameSource);
-
-
-#find and write new game index and new source index
-gameIndex = gameIndex + 4; #for the , and the space and "x2
-gameIndex = gameIndex + len(gameTitle);
-
-sourceIndex = sourceIndex + 4; #for the , and the space and 'x2
-sourceIndex = sourceIndex + len(gameSource);
-
-referenceFile.seek(0);
-referenceFile.write(str(gameIndex));
-referenceFile.write('-');
-referenceFile.write(str(sourceIndex));
-
-gameFile.close();
-referenceFile.close();
-
-
-'''
-print("Done");
+writeTitleAndSource();
+writeCategoryAndThumbnail();
