7d987c257770b8453846055e2f75469ffc5b00fc
suhussai <suhussai@ualberta.ca>
2014-12-25 11:49:06 -0700

    jquery version in order to congregate all necessary script files and in order to make minifying easy

--- clickerFunction.js
+++ clickerFunction.js
@@ -3,48 +3,51 @@ var w = screen.width;
 var selectionBoxWidth = w*0.30;
 var gameBoxWidth = w - selectionBoxWidth;
 
-var gameBoxController = document.getElementById("gameBox");
-gameBoxController.width = gameBoxWidth;
-gameBoxController.height = h;
+//var gameBoxController = document.getElementById("gameBox");
+//gameBoxController.width = gameBoxWidth;
+//gameBoxController.height = h;
+
+$("#gameBox").width = gameBoxWidth;
+$("#gameBox").height = h;
 
 function hideAll() {
     
     for (i = 1; i < categories.length; i++) {
 	if (displayed[i] == 1) {
-	    var selectionBox = document.getElementById(gameSelectionBox[i]);
-	    selectionBox.style.float = "left";
-	    selectionBox.style.border = "0px red solid";
-	    selectionBox.style.left = "0%";
-	    selectionBox.style.top = "0%";
-	    selectionBox.style.height = h;
-	    selectionBox.style.width = selectionBoxWidth;
-	    selectionBox.style.visibility = "hidden";
-	    selectionBox.style.position = "absolute";
+	    //	    var selectionBox = document.getElementById(gameSelectionBox[i]);
+	    $("#"+gameSelectionBox[i]).css("float","left");
+	    $("#"+gameSelectionBox[i]).css("border", "0px red solid");
+	    $("#"+gameSelectionBox[i]).css("left","0%");
+	    $("#"+gameSelectionBox[i]).css("top","0%");
+	    $("#"+gameSelectionBox[i]).css("height", h);
+	    $("#"+gameSelectionBox[i]).css("width", selectionBoxWidth);
+	    $("#"+gameSelectionBox[i]).css("visibility", "hidden");
+	    $("#"+gameSelectionBox[i]).css("position", "absolute");
 	    displayed[i] = 0;
 	}
     }
 }
 
 function showOne(i) {
-    var selectionBox = document.getElementById(gameSelectionBox[i]);
-    selectionBox.style.visibility = "visible"; 
-    selectionBox.style.float = "left";
-    selectionBox.style.border = "0px red solid";
-    selectionBox.style.left = "0%"; 
-    selectionBox.style.top = "0%"; 
-    selectionBox.style.height = h; 
-    selectionBox.style.width = selectionBoxWidth; 
-    selectionBox.style.position = "absolute";
+//    var selectionBox = document.getElementById(gameSelectionBox[i]);
+    $("#"+gameSelectionBox[i]).css("float","left");
+    $("#"+gameSelectionBox[i]).css("border", "0px red solid");
+    $("#"+gameSelectionBox[i]).css("left","0%");
+    $("#"+gameSelectionBox[i]).css("top","0%");
+    $("#"+gameSelectionBox[i]).css("height", h);
+    $("#"+gameSelectionBox[i]).css("width", selectionBoxWidth);
+    $("#"+gameSelectionBox[i]).css("visibility", "visible");
+    $("#"+gameSelectionBox[i]).css("position", "absolute");
     displayed[i] = 1;
     
     for (j = 1; j < gameSrc[i].length; j++) {
-	document.getElementById("img"+i+j).src = gameSrc[i][j];
-	document.getElementById("ling"+i+j).href = "play.html?name=" +  gameTitles[i][j];
+	$("#img"+i+j).attr("src", gameSrc[i][j]);
+	$("#ling"+i+j).attr("href", "play.html?name=" +  gameTitles[i][j]);
     }
 }
 
 function clickCheck(i) {
-    document.getElementById(categories[i]).onclick = function(){
+    $("#" + categories[i]).click(function(){
 	if (displayed[i] == 0) {
             hideAll();
             showOne(i);
@@ -52,9 +55,9 @@ function clickCheck(i) {
 	else {
 	    hideAll();
         }
-    };
+    });
     
-    document.getElementById(titles[i]).onclick = function(){
+    $("#" + titles[i]).click(function(){
 	if (displayed[i] == 0) {
             hideAll();
             showOne(i);
@@ -62,7 +65,7 @@ function clickCheck(i) {
 	else {
 	    hideAll();
         }
-    };
+    });
 
 
 
